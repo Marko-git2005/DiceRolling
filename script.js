102ba1818658822df12d067c300dc9431a053f10
Marko Angelovski <112504911+Marko-git2005@users.noreply.github.com>
2024-01-14 19:29:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ function rollDice(){
+    
+    const numofDice = document.getElementById("#ofdice").value; 
+    const diceResult = document.getElementById("result"); 
+    const diceimages = document.getElementById("images"); 
+    const values = []; 
+    const images = []; 
+
+    for(let i =0; i <numofDice; i++){
+        const value = Math.floor(Math.random()*6) +1; 
+        console.log(value); 
+        values.push(value);
+        images.push(`<img src="images/${value}.png">`); 
+    }
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceimages.innerHTML = images.join('');  
+}
